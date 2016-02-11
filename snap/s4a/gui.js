d3f47c936691d9cb864753b92aaa70a5dc3a87ab
Bernat Romagosa <tibabenfortlapalanca@gmail.com>
2016-02-11 17:16:36 +0100

    messaging protocol is now 100% consistent with S4A's

--- snap/s4a/gui.js
+++ snap/s4a/gui.js
@@ -24,76 +24,107 @@ IDE_Morph.prototype.init = function(globals) {
 }
 
 IDE_Morph.prototype.handleHTTPRequest = function(request, response) {
+    var myself = this;
 
-    if (request.url.length > 0) {
-        var path = request.url.slice(1),
-            command = path.split('=');
-        switch (command[0]) {
-            case 'broadcast':
-                var message = command[1],
-                    stage = this.stage,
-                    hats = [];
+    response.setHeader('Access-Control-Allow-Origin', '*');
 
-                if (message.length > 0) {
-                    
-                    stage.lastMessage = message;
+    function parse(message) {
 
-                    stage.children.concat(stage).forEach(function (morph) {
+        if (message.length > 0) {
+
+            // If the command came inside a POST body, it needs to be split by spaces,
+            // otherwise it came in a GET URL and it needs to be split by "="
+
+            var command = message.indexOf('=') > 0 ? message.split('=') : message.split(' ');
+
+            switch (command[0]) {
+                case 'broadcast':
+                    var message = command[1],
+                        stage = myself.stage,
+                        hats = [];
+
+                    if (message.length > 0) {
+
+                        stage.lastMessage = message;
+
+                        stage.children.concat(stage).forEach(function (morph) {
+                            if (morph instanceof SpriteMorph || morph instanceof StageMorph) {
+                                hats = hats.concat(morph.allHatBlocksFor(message));
+                            }
+                        });
+
+                        hats.forEach(function (block) {
+                            stage.threads.startProcess(
+                                    block,
+                                    stage.isThreadSafe
+                                    )});
+
+                        response.end('broadcast ' + message);
+
+                    } else {
+                        response.end('no message provided');
+                    }
+                    break;
+
+                case 'vars-update':
+                    var varName = command[1],
+                        value = command[2],
+                        stage = myself.stage;
+
+                    stage.globalVariables().setVar(varName, value, stage);
+                    response.end('updating variable ' + command[1] + ' to value ' + command[2]);
+                    break;
+
+                case 'send-messages':
+                    var contents = 'broadcast',
+                        stage = myself.stage;
+
+                    stage.children.concat(stage).forEach(function(morph) {
                         if (morph instanceof SpriteMorph || morph instanceof StageMorph) {
-                            hats = hats.concat(morph.allHatBlocksFor(message));
+                            morph.allMessageNames().forEach(function(message) {
+                                contents += ' "' + message + '"';
+                            });
                         }
                     });
 
-                    hats.forEach(function (block) {
-                        stage.threads.startProcess(
-                                block,
-                                stage.isThreadSafe
-                                )});
+                    response.end(contents);
+                    break;
 
-                    response.end('broadcasting ' + message);
+                case 'send-vars':
+                    var contents = 'sensor-update',
+                        stage = myself.stage;
 
-                } else {
-                    response.end('no message provided');
-                }
-                break;
-            case 'vars-update':
-                var varName = command[1],
-                    value = command[2],
-                    stage = this.stage;
+                    Object.keys(stage.globalVariables().vars).forEach(function(varName) {
+                        contents += ' "' + varName + '" ' + stage.globalVariables().vars[varName].value;
+                    });
 
-                stage.globalVariables().setVar(varName, value, stage);
-                response.end('updating variable ' + command[1] + ' to value ' + command[2]);
-                break;
-            case 'send-messages':
-                var contents = 'broadcast',
-                    stage = this.stage;
-
-                stage.children.concat(stage).forEach(function(morph) {
-                    if (morph instanceof SpriteMorph || morph instanceof StageMorph) {
-                        morph.allMessageNames().forEach(function(message) {
-                            contents += ' "' + message + '"';
-                        });
-                    }
-                });
-
-                response.end(contents);
-                break;
-            case 'send-vars':
-                var contents = 'sensor-update',
-                    stage = this.stage;
-
-                Object.keys(stage.globalVariables().vars).forEach(function(varName) {
-                    contents += ' "' + varName + '" ' + stage.globalVariables().vars[varName].value;
-                });
-
-                response.end(contents);
-                break;
+                    response.end(contents);
+                    break;
+            }
         }
+    }
+
+    if (request.method === 'POST') {
+
+        var body = '';
+
+        request.addListener('data', function(chunk) {
+            body += chunk;
+        });
+
+        request.addListener('end', function(chunk){
+            if (chunk) { 
+                body += chunk;
+            }
+            parse(body);
+        });
+
+    } else if (request.method === 'GET') {
+        parse(request.url.slice(1));
     }
 
     response.end('Unknown command');
 }
-
 
 // Override Snap! menus
 // ToDo: Duplicate code! This is terrible style... we need to think of a better way 
