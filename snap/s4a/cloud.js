abd5034f5dde37ef318ffdb69e0b92c7bdde3da1
Bernat Romagosa <tibabenfortlapalanca@gmail.com>
2016-05-30 15:24:46 +0200

    fixes #1

--- snap/s4a/cloud.js
+++ snap/s4a/cloud.js
@@ -1,3 +1,25 @@
 var SnapCloud = new Cloud(
     'https://snap.apps.miosoft.com/SnapCloudLocal'
     );
+
+Cloud.prototype.overrideUrl = function () {
+    this.url = 'https://snap.apps.miosoft.com/SnapCloud';
+};
+
+Cloud.prototype.revertUrl = function () {
+    this.url = 'https://snap.apps.miosoft.com/SnapCloudLocal';
+};
+
+Cloud.prototype.originalSignup = Cloud.prototype.signup;
+Cloud.prototype.signup = function (username, email, callBack, errorCall) {
+    this.overrideUrl();
+    this.originalSignup(username, email, callBack, errorCall);
+    this.revertUrl();
+};
+
+Cloud.prototype.originalResetPassword = Cloud.prototype.resetPassword;
+Cloud.prototype.resetPassword = function (username, callBack, errorCall) {
+    this.overrideUrl();
+    this.originalResetPassword(username, callBack, errorCall);
+    this.revertUrl();
+}
