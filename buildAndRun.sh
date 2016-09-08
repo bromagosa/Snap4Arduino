b50a52b42b2f8c121d14515b42f85fbe6525a206
bromagosa <bromagosa@e-citilab.eu>
2016-09-08 18:41:55 +0200

    working on a multiplatform repo

--- buildAndRun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd snap
-cp -R ~/bin/nw/node_modules .
-zip -r app.nw *
-mv app.nw /tmp
-rm -R node_modules
-cd ..
-./run.sh
--- updateSnap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm master.zip 2> /dev/null
-wget https://github.com/jmoenig/Snap--Build-Your-Own-Blocks/archive/master.zip
-unzip master.zip
-rm master.zip 
-cp -rf Snap--Build-Your-Own-Blocks-master/* snap/
-rm -r Snap--Build-Your-Own-Blocks-master
