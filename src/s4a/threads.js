c741a61a670c34bc26b2a0f84132849c6119fd4f
Joan Guillén i Pelegay <jguille2@xtec.cat>
2017-06-20 15:21:23 +0200

    Fixing bug in first readings

--- src/s4a/threads.js
+++ src/s4a/threads.js
@@ -136,22 +136,12 @@ Process.prototype.reportAnalogReading = function (pin) {
 
         if (board.pins[board.analogPins[pin]].mode != board.MODES.ANALOG) {
             board.pinMode(board.analogPins[pin], board.MODES.ANALOG);
-        }
-
-        // Ugly hack that fixes issue #5
-        // "say" block inside a "forever" loop shows only first reading on GNU/Linux and MS-Windows
-        // Until we find the source of the problem and a cleaner solution...
-
-        if (!this.context.justRead) {
-            this.context.justRead = true;
         } else {
-            this.context.justRead = false;
             return board.pins[board.analogPins[pin]].value;
         }
 
         this.pushContext('doYield');
         this.pushContext();
-
     } else {
         throw new Error(localize('Arduino not connected'));	
     }
@@ -161,13 +151,18 @@ Process.prototype.reportDigitalReading = function (pin) {
     var sprite = this.homeContext.receiver;
 
     if (sprite.arduino.isBoardReady()) {
+
         var board = sprite.arduino.board; 
 
         if (board.pins[pin].mode != board.MODES.INPUT) {
             board.pinMode(pin, board.MODES.INPUT);
-            board.digitalRead(pin, function(value) { board.pins[pin].value = value });
+            board.reportDigitalPin(pin, 1);
+        } else {
+            return board.pins[pin].value == 1;
         }
-        return board.pins[pin].value == 1;
+
+        this.pushContext('doYield');
+        this.pushContext();
     } else {
         throw new Error(localize('Arduino not connected'));		
     }
