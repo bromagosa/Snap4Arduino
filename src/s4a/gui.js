7418b575a3f71bfe193d6a8f685394ca0eb2c6c6
Joan Guillén i Pelegay <jguille2@xtec.cat>
2022-01-04 01:05:36 +0100

    Updating Snap! to 7.0.3 dev

--- src/s4a/gui.js
+++ src/s4a/gui.js
@@ -163,7 +163,7 @@ IDE_Morph.prototype.aboutSnap = function () {
         module, btn1, btn2, btn3, btn4, licenseBtn, translatorsBtn,
         world = this.world();
 
-    aboutTxt = 'Snap! 7.0.2 - dev -\nBuild Your Own Blocks\n\n'
+    aboutTxt = 'Snap! 7.0.3 - dev -\nBuild Your Own Blocks\n\n'
         + 'Copyright \u24B8 2008-2021 Jens M\u00F6nig and '
         + 'Brian Harvey\n'
         + 'jens@moenig.org, bh@cs.berkeley.edu\n\n'
@@ -386,7 +386,7 @@ IDE_Morph.prototype.aboutSnap4Arduino = function () {
         aboutTxt = 'Snap4Arduino ' + version +'\n'
         + 'http://snap4arduino.rocks\n\n'
 
-        + 'Copyright \u24B8 2018-2021 Bernat Romagosa and Joan Guillén\n'
+        + 'Copyright \u24B8 2018-2022 Bernat Romagosa and Joan Guillén\n'
         + 'https://github.com/bromagosa/snap4arduino\n\n'
 
         + 'Copyright \u24B8 2016-2017 Bernat Romagosa and Arduino.org\n\n'
