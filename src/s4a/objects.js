649a844a603e3499936172f6f552caf8e9ff572f
Joan Guillén i Pelegay <jguille2@xtec.cat>
2023-05-18 23:03:10 +0200

    fixing problems with ditigal reporting

--- src/s4a/objects.js
+++ src/s4a/objects.js
@@ -528,19 +528,21 @@ SpriteMorph.prototype.reportAnalogReading = function (pin) {
 };
 
 SpriteMorph.prototype.reportDigitalReading = function (pin) {
-    if (this.arduino.isBoardReady()) {
+    if (this.arduino.isBoardReady() && pin) {
         var board = this.arduino.board;
-
-        if (!pin) { return false };
-
         if (board.pins[pin].mode != board.MODES.INPUT) {
             board.pinMode(pin, board.MODES.INPUT);
-            board.reportDigitalPin(pin, 1);
+            board.pins[pin].report = 1;
+        } else {
+            if (board.pins[pin].report == 1) {
+                board.reportDigitalPin(pin, 1);
+                board.pins[pin].report = 0;
+            } else {
+                return board.pins[pin].value == 1;
+            }
         }
-        return board.pins[pin].value == 1;
-    } else {
-        return false;
     }
+    return false;
 };
 
 SpriteMorph.prototype.arduinoWatcher = function (selector, label, color, pin) {
